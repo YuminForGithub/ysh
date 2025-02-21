b5a9159eb6f0b9d64e810e73c330ffcef7ede5ef
YuminForGithub <142571383+YuminForGithub@users.noreply.github.com>
2025-02-21 13:56:36 +0900

    Create yshOSbuild.py
    OS module ysh runner function Builder created

---
+++ yshOSbuild.py
@@ -0,0 +1,17 @@
+code = """\
+import os
+
+def ysh_run():
+    print("Hello from my function!")
+
+# Add the function to the os module
+os.run_ysh = ysh_run
+"""
+
+print("write/creating the code...")
+
+# Create (or overwrite) the file named 'sitecustomize.py'
+with open("sitecustomize.py", "w") as file:
+  file.write(code)
+
+print("sitecustomize.py has been created successfully!")
